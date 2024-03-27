5e159d67718bb98bde16a28c56fa2209e3f5de9e
eli <eli.chandler@gmail.com>
2024-03-28 04:50:09 +1300

    add dotenv as frontend requirement

--- frontend/src/api/wordsApi.ts
+++ frontend/src/api/wordsApi.ts
@@ -1,8 +1,5 @@
 import axios from 'axios';
 import { WordDTO } from "../props/Word";
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const baseUrl = process.env.REACT_APP_API_URL || "http://localhost:3000";
 
